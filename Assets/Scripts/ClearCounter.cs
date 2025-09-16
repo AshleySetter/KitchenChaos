eefb17220e88b03dab98ae89114bde1a953abf03
Ash <assetter@hotmail.com>
2025-09-16 22:23:39 +0200

    implement player placing objects on and picking up objects from clear counters

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -11,7 +11,32 @@ public class ClearCounter : BaseCounter, IKitchenObjectParent
 
     public override void Interact(Player player)
     {
-
+        if (!HasKitchenObject())
+        {
+            // no kitchen object on counter
+            if (player.HasKitchenObject())
+            {
+                // player carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Player not carrying anything
+            }
+        }
+        else
+        {
+            // there is a kitchen object on counter
+            if (player.HasKitchenObject())
+            {
+                // the player is carrying something
+            }
+            else
+            {
+                // player not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 
 }

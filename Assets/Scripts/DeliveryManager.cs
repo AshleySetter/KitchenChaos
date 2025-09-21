26c65944e3d7db167f78a9576ea4b979187d4fc5
Ash <assetter@hotmail.com>
2025-09-21 19:07:18 +0200

    add sound manager and play sounds on successful and failed delivery at delivery counter position using event handler

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -7,6 +7,8 @@ public class DeliveryManager : MonoBehaviour
 {
     public event EventHandler OnRecipeSpawned;
     public event EventHandler onRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFail;
 
     public static DeliveryManager Instance { get; private set; }
 
@@ -76,12 +78,15 @@ public class DeliveryManager : MonoBehaviour
                     Debug.Log("Player delivered a correct recipe");
                     waitingRecipeSOList.RemoveAt(i);
                     onRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
         }
         // No Matches found! Player delivered an incorrect recipe which wasn't being waited for
         Debug.Log("Player delivered a recipe nobody was waiting for");
+        OnRecipeFail?.Invoke(this, EventArgs.Empty);
+
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList()
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClipsRefsSO audioClipsRefsSO;
+
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    {
+        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+    }
+
+    private void PlaySound(AudioClip[] audioClips, Vector3 position, float volume = 1f)
+    {
+        PlaySound(audioClips[UnityEngine.Random.Range(0, audioClips.Length)], position, volume);
+    }
+
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFail += DeliveryManager_OnRecipeFail;
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, EventArgs e)
+    {
+        DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
+        PlaySound(audioClipsRefsSO.deliverySuccess, deliveryCounter.transform.position);
+    }
+
+    private void DeliveryManager_OnRecipeFail(object sender, EventArgs e)
+    {
+        DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
+        PlaySound(audioClipsRefsSO.deliveryFail, deliveryCounter.transform.position);
+    }
+}

cc24ba01477d2d6d8a32e226fda74a433f56c618
Ash <assetter@hotmail.com>
2025-09-21 22:15:08 +0200

    Implement basic game manager with 4 states, show countdown to start of the game (interaction locked until game start)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -44,6 +44,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
+        if (!GameManager.Instance.IsGamePlaying()) return;
         if (selectedCounter != null)
         {
             selectedCounter.Interact(this);
